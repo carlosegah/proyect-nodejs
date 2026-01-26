69bed121f46a2ab8fc25652050fd073abcdb7432
Carlos Gallardo <c4rlosgallardo@gmail.com>
2026-01-26 03:05:40 -0300

    actualizacion

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 import express from 'express';
 const app = express();
 
-
-app.use((req, res, next) => {
-    console.log(req.method);
-    next();
-});
+const products = [
+    {
+        id: 1,
+        name: "Camiseta deportiva",
+        price: 15000,
+        categories: ['ropa', 'deportes', 'entrenamiento'],
+    },
+    {
+        id: 2,
+        name: "Zapatos deportivos",
+        price: 97000,
+        categories: ['calzado', 'deportes', 'entrenamiento'],        
+    },
+    {
+        id: 3,
+        name: "Auricular bluetooth",
+        price: 23000,
+        categories: ['auricular', 'vincha', 'multimedia'],
+    },
+    {
+        id: 4,
+        name: "Medias",
+        price: 3000,
+        categories: ['calzado', 'deportes', 'entrenamiento'],
+    },
+    {
+        id: 5,
+        name: "Smartband",
+        price: 55000,
+        categories: ['Smartbands & Smartwatchs', 'tecnologia wereable', 'Electrónica'],
+    },
+    {
+        id: 6,
+        name: "Celular: Motorola E68",
+        price: 235000,
+        categories: ['Celulares', 'Ofertas', 'Electrónica'],
+    }
+]
 
 app.get('/', (req, res) => {
     res.json({
         message: "Bienvenidos a mi API REST"
     });
 });
+
+app.use((request, response, next) => {
+    response.json( {
+        message: "Hola, este es mi primer API REST" 
+    })
+})
 
 import notFound from './src/middlewares/not-found.js';
 app.use(notFound);
